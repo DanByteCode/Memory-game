31c64851c6c08d5eece229a7ad14f4cd7da71d33
Darknhyt <40185738+Darknhyt@users.noreply.github.com>
2024-07-21 16:00:59 -0400

    Adding animations

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,21 +1,22 @@
-import { randomSecuence } from "./utils"
-import { useState } from "react"
+import { AnimatePresence, motion } from 'framer-motion'
+import { randomSecuence } from './utils'
 
-const PREFIX = "https://ddragon.leagueoflegends.com/cdn/img/champion/loading/"
+const PREFIX = 'https://ddragon.leagueoflegends.com/cdn/img/champion/loading/'
 let marked = []
 
-export default function Game({ modifyScore, champList }) {
-  const [list, setList] = useState(champList)
+export default function Game({ modifyScore, champList, setList }) {
   function playState(e, info) {
     e.preventDefault()
-    if (marked.some(m => m == info)) {
+    if (marked.some((m) => m == info)) {
       modifyScore(false)
       marked = []
-      e.target.classList.add("fail")
-      setTimeout(async () => { e.target.classList.remove("fail") }, 500)
+      e.target.classList.add('fail')
+      setTimeout(async () => {
+        e.target.classList.remove('fail')
+      }, 500)
     } else {
       modifyScore(true)
-      const newList = randomSecuence(champList.length).map((v) => list[v])
+      const newList = randomSecuence(champList.length).map((v) => champList[v])
       marked.push(info)
       setList(newList)
     }
@@ -23,13 +24,30 @@ export default function Game({ modifyScore, champList }) {
 
   return (
     <main>
-      {list.map((data) => {
-        return <div key={data.id} className="card" onClick={(e) => playState(e, data.id)}>
-          <img src={`${PREFIX}${data.id}_0.jpg`}
-            alt={data.name} title={`${data.name} ${data.title}`}
-            width="300" />
-          <span>{data.name}</span>
-        </div>
-      })}
-    </main>)
+      <AnimatePresence mode="popLayout">
+        {champList.map((data) => {
+          return (
+            <motion.div
+              initial={{ width: 0, opacity: 0 }}
+              animate={{ width: '100%', opacity: 1 }}
+              exit={{ width: 0, opacity: 0 }}
+              layout
+              layoutId={data.id}
+              key={data.id}
+              className="card"
+              onClick={(e) => playState(e, data.id)}
+            >
+              <img
+                src={`${PREFIX}${data.id}_0.jpg`}
+                alt={data.name}
+                title={`${data.name} ${data.title}`}
+                width="300"
+              />
+              <span>{data.name}</span>
+            </motion.div>
+          )
+        })}
+      </AnimatePresence>
+    </main>
+  )
 }
